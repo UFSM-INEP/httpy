992e64094877383f979f2cf56642f32070240284
Marcos Visentini <marcosvisentini@gmail.com>
2023-12-13 13:57:33 -0300

    Remove parâmetro 'encoding' da configuração de logging para suportar versões menores do Python

--- httpy.py
+++ httpy.py
@@ -12,8 +12,7 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] [%(levelname)8s]: %(message)s',
-    datefmt='%d/%m/%Y %H:%M:%S',
-    encoding='UTF-8',
+    datefmt='%d/%m/%Y %H:%M:%S'
 )
 
 logger = logging.getLogger(__name__)
